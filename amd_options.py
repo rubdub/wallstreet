8cf1d468f8eaa9f046c63a593de0e96344e92708
rubdub <rubdub555@gmail.com>
2017-07-08 21:40:07 -0500

    Kind of working P/L calculation from greeks.

--- amd_options.py
+++ amd_options.py
@@ -33,42 +33,75 @@ def price_range(low, high):
     return price_list
 
 
-stored_price_range = price_range(12, 16)
+def future_option_price(option, tgt_days, tgt_price, tgt_iv):
+    ask = option.ask
+    price = option.underlying.price
+    iv = option.implied_volatility()
+    delta = option.delta()
+    gamma = option.gamma()
+    theta = option.theta()
+    vega = option.vega()
+    rho = option.rho()
+
+    print(ask)
+    print(iv)
+
+    final_price_delta = delta * (tgt_price-price)
+    final_price_gamma = (gamma*delta)*(tgt_price-price)
+    final_price_theta = (-1)*tgt_days*theta
+    final_price_vega = (tgt_iv-iv)*vega
+    final_price_rho = 0 # Not sure how to get the "Risk-Free Rate Of Return" from yahoo finance"
+    final_price = ask+final_price_delta+final_price_gamma+final_price_theta+final_price_vega+final_price_rho
+    return final_price
+
+# stored_price_range = price_range(12, 16)
 
 # c = Call('AMD', source='yahoo')
 # print(c.expirations)
 
+# price = []
+# for strike_price in stored_price_range:
+#     g = Call('AMD', d=4, m=8, y=2017, strike=strike_price, source='yahoo')
+#     price.append(g.ask)
+#
+# print(price)
+#
+# plt.scatter(stored_price_range, price, color='red', marker='.', alpha=0.5, s=400)
+# plt.show()
+
+fut_opt13 = Call('AMD', d=4, m=8, y=2017, strike=13, source='yahoo')
+
+# print(future_option_price(fut_opt13, 30, 17, .8906))
+
 price = []
-for strike_price in stored_price_range:
-    g = Call('AMD', d=4, m=8, y=2017, strike=strike_price, source='yahoo')
-    price.append(g.ask)
+for i in range(10, 17):
+    option_price = future_option_price(fut_opt13, 1, i, .8906)
+    price.append(option_price)
 
-print(price)
-
-plt.scatter(stored_price_range, price, color='red', marker='.', alpha=0.5, s=400)
+plt.scatter(range(10, 17), price, color='red', marker='.', alpha=0.5, s=400)
 plt.show()
 
 
-S0 = 13.36
-K = 17
-r=0.15
-sigma = 87.5
-T = 32/365.0
-Otype='C'
-
-
-
-print("S0\tstock price at time 0:", S0)
-print("K\tstrike price:", K)
-print("r\tcontinuously compounded risk-free rate:", r)
-print("sigma\tvolatility of the stock price per year:", sigma)
-print("T\ttime to maturity in trading years:", T)
-
-
-t=time.time()
-c_BS = BlackScholes(Otype,S0, K, r, sigma, T)
-elapsed=time.time()-t
-print("c_BS\tBlack-Scholes price:", c_BS, elapsed)
+# S0 = 13.36
+# K = 17
+# r=0.15
+# sigma = 87.5
+# T = 32/365.0
+# Otype='C'
+#
+#
+#
+# print("S0\tstock price at time 0:", S0)
+# print("K\tstrike price:", K)
+# print("r\tcontinuously compounded risk-free rate:", r)
+# print("sigma\tvolatility of the stock price per year:", sigma)
+# print("T\ttime to maturity in trading years:", T)
+#
+#
+# t=time.time()
+# c_BS = BlackScholes(Otype,S0, K, r, sigma, T)
+# elapsed=time.time()-t
+# print("c_BS\tBlack-Scholes price:", c_BS, elapsed)
 
 
 
